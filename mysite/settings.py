4c9ef3a84a30c04782e360d624d0e35099d24a9f
elcin <elcin06@hacettepe.edu.tr>
2019-08-26 05:34:28 +0300

    false u geri true yaptım, allowed host u elcineksi. yaptım.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5%m6-ws@ofa*z=vcaql9olh@h##4i&-(6svf0a^bgqtyj9au@3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'elcineksi.pythonanywhere.com']
 
 
 # Application definition
